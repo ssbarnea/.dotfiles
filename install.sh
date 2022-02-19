559f207fe01e651a4ddbc87aeef7f7430360f1d1
Sorin Sbarnea <ssbarnea@redhat.com>
2022-02-19 10:55:48 +0000

    Added brew install and restore

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ex
 
+# Quickly install all brew dependencies from Brewfile
+brew bundle --no-upgrade --no-lock
+
 # Relinking dotfiles
 stow -v root hush
 
@@ -9,3 +12,8 @@ pip install -q -r requirements.txt
 
 # Install npm dependencies
 cat npm.txt | grep -v '^#' | xargs npm install -g
+
+# Configure background updates if needed
+brew autoupdate status | grep 'and running' >/dev/null || {
+    brew autoupdate --upgrade start 86400 --enable-notification
+}
