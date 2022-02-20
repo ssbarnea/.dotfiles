e11ca00fe7b4dd92599c590c06e96f7bf97e4e86
Sorin Sbarnea <ssbarnea@redhat.com>
2022-02-20 10:05:08 +0000

    Add pipeline to test install

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 set -ex
 
 # Quickly install all brew dependencies from Brewfile
-brew bundle --no-upgrade --no-lock
+brew bundle --no-upgrade --no-lock --file Brewfile.min.rb
 
 # Relinking dotfiles
 stow -v root hush
 
 # Installing python dependencies
-pip install -q -r requirements.txt
+pip3 install -q -r requirements.txt
 
 # Install npm dependencies
 grep -v '^#' npm.txt  | xargs npm install -g
@@ -17,3 +17,8 @@ grep -v '^#' npm.txt  | xargs npm install -g
 brew autoupdate status | grep 'and running' >/dev/null || {
     brew autoupdate --upgrade start 86400 --enable-notification
 }
+
+# Avoid installing the whole brew bundle unless on interactive shell
+if tty -s; then
+    brew bundle --no-upgrade --no-lock
+fi
